379faeff53d53e1d154dbc5840c32491b094250c
JeanV155 <carlosgutierrez203074@gmail.com>
2026-02-11 12:30:50 -0500

    fffgg123

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,7 +5,18 @@
 </template>
 
 <script setup>
-
+DumplingArray = [
+  {name: 'lamd', traditional: 'very'}
+  {name: 'beef', traditional: 'very'}
+  {name: 'chicken', traditional: 'very'}
+  {name: 'peppers', traditional: 'very'}
+  {name: 'soup', traditional: 'kinda'}
+  {name: 'tofu', traditional: 'kinda'}
+  {name: 'pork', traditional: 'very'}
+  {name: 'spinach', traditional: 'kinda'}
+  {name: 'cherry', traditional: 'not really'}
+  {name: 'shrimp', traditional: 'kinda'}
+]
 
 </script>
 
