56f182048a88559490bb9c87f44ab531ae4de5d0
JeanV155 <carlosgutierrez203074@gmail.com>
2026-03-05 12:30:51 -0500

    tttt334

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,31 +5,42 @@
 <img src="" alt="">
   </div>
   <fillingcard v-for="filling in fillings " :key="filling.name" :filling = "filling">
-    <button @click="cartAdd">{{ filling.price }}</button>
+    <button @click="cartAdd(filling.price)">{{ filling.price }} USD</button>
   </fillingcard>
+ 
   
 </template>
 
 <script setup>
 
 import fillingcard from '@/components/cardsview.vue'
-
+import { ref, render, toDisplayString } from 'vue'
 const fillings = [
-  {name: 'lamb', traditional: 'very', price: '5 USD',}
-  ,{name: 'beef', traditional: 'very', price: '7 USD'}
-  ,{name: 'chicken', traditional: 'very', price: '10 USD'}
-  ,{name: 'peppers', traditional: 'very', price: '2 USD'}
-  ,{name: 'soup', traditional: 'kinda', price: '4 USD'}
-  ,{name: 'tofu', traditional: 'kinda', price: '1 USD'}
-  ,{name: 'pork', traditional: 'very', price:'8 USD'}
-  ,{name: 'spinach', traditional: 'kinda', price: '1 USD'}
-  ,{name: 'cherry', traditional: 'not really', price: '1 USD'}
-  ,{name: 'shrimp', traditional: 'kinda', price: '6 USD'}
+  {name: 'lamb', traditional: 'very', price: 5}
+  ,{name: 'beef', traditional: 'very', price: 7}
+  ,{name: 'chicken', traditional: 'very', price: 10 }
+  ,{name: 'peppers', traditional: 'very', price: 2 }
+  ,{name: 'soup', traditional: 'kinda', price: 4}
+  ,{name: 'tofu', traditional: 'kinda', price: 1}
+  ,{name: 'pork', traditional: 'very', price:8}
+  ,{name: 'spinach', traditional: 'kinda', price: 1}
+  ,{name: 'cherry', traditional: 'not really', price: 1}
+  ,{name: 'shrimp', traditional: 'kinda', price: 6 }
 ]
 
 
 const price = ref([])
-function orderAdd(idea){}
+const total = ref(0)
+
+function cartAdd(idea){
+  price.value.push(idea)
+total =+ fillings.price
+renderprice();
+  
+}
+
+
+
 
 </script>
 
